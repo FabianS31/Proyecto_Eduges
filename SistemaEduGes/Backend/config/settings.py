216f7b13fc838f19f37a46fa43ae30eab6ba27a6
Roman <romanforte8@gmail.com>
2026-09-04 17:44:27 -0300

    feat: prototipo de login y dashboard con estilos institucionales

--- SistemaEduGes/Backend/config/settings.py
+++ SistemaEduGes/Backend/config/settings.py
@@ -54,10 +54,11 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -116,6 +117,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
