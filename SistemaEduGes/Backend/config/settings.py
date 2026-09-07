548119f27bc2ba5b3f68c81968db14088660f33a
Roman <romanforte8@gmail.com>
2026-09-07 10:21:45 -0300

    chore: ajuste de settings para entorno local con sqlite

--- SistemaEduGes/Backend/config/settings.py
+++ SistemaEduGes/Backend/config/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'EduGes',
-        'USER': 'postgres',
-        'PASSWORD': '98i4ji',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
